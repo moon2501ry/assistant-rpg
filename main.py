04fffbcafabd38db05ef7e1c373893b2c6cf7a3c
Ryan Amorim <159044164+moon2501ry@users.noreply.github.com>
2025-04-04 15:56:48 -0300

    Add Text to Speech

--- main.py
+++ main.py
@@ -29,11 +29,11 @@ def play_audio(audio:str|None="audio.mp3"):
 
 pygame.mixer.init();
 
-config = ConfigTXT({"API-KEY":"str,none", "DIR-SOUNDPAD":"str,none"});
+config = ConfigTXT({"API-KEY":"str,none", "DIR-SOUNDPAD":"str,none", "VOICE-ID":"str,none"});
 assistant = AssistAI("Você será um assistente para RPGs");
 while True:
     os.system('cls' if os.name == 'nt' else 'clear');
-    _input = input("Lista de comandos:\n  assist [msg] - Pergunte para o assistente;\n  setperson [name_or_id] - Seleciona um personagem;\n  person [msg] - Imita o personagem selecionado;\n  d [num_of_dice] [num_dice] - Rola [num_of_dice] dados de [num_dice] dados;\n\nInput> ");
+    _input = input("Lista de comandos:\n  assist [msg] - Pergunte para o assistente;\n  setperson [name_or_id] - Seleciona um personagem;\n  person [msg] - Imita o personagem selecionado;\n  textaudio [text] - Faz a voz de um npc;\n  d [num_of_dice] [num_dice] - Rola [num_of_dice] dados de [num_dice] dados;\n\nInput> ");
     os.system('cls' if os.name == 'nt' else 'clear');
     match _input.split()[0]:
         case "assist":
@@ -56,7 +56,6 @@ while True:
                     play_audio();
                     print(f"{person.name}:", response);
             except:
-                os.system('cls' if os.name == 'nt' else 'clear');
                 print("Nenhum personagem selecionado!");
         case "d":
             dice_roll = [];
@@ -76,6 +75,19 @@ while True:
             dice_print += f"| [{dice_result}] foi o resultado!";
             save_dice(seed, dice_result);
             print(dice_print);
+        case "textaudio":
+            try:
+                try:
+                    person_ai.text_to_speech(person.voice, get_param(_input));
+                    person_ai.save_audio();
+                    play_audio();
+                except:
+                    speech = AssistAI(chat_save=False, api_key=config.get("API-KEY"));
+                    speech.text_to_speech(config.get("VOICE-ID"), get_param(_input));
+                    speech.save_audio();
+                    play_audio();
+            except:
+                print("API-KEY not seted or VOICE-ID not seted!")
         case _:
             print("Comando não existe");
     input();
